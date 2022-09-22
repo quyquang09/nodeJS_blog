51c35688cabb1aaf85d3b810636155666e0a86c8
quyquang09 <103199747+quyquang09@users.noreply.github.com>
2022-09-22 14:54:11 +0700

    handlebars,boostrap4,

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan')
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.use(morgan('combined'))
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/news', (req, res) => {
+    res.render('news');
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
