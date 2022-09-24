4cf8b0b53b7d3bb1dc53172f7adca2ef239b6857
quyquang09 <103199747+quyquang09@users.noreply.github.com>
2022-09-24 23:27:19 +0700

    Install Prettier, Lint-staged, husky

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,11 @@
+class NewsController {
+	//[GET] /news
+	index(req, res) {
+		res.render('news');
+	}
+	//[GET] /new/:slug
+	show(req, res) {
+		res.send('News Detail');
+	}
+}
+module.exports = new NewsController();
--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 const path = require('path');
 const express = require('express');
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const exphbs = require('express-handlebars');
+const router = require('./routes');
 
 const app = express();
 const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.use(morgan('combined'))
-app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+router(app);
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
